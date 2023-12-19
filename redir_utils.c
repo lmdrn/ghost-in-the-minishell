a0796a56bad937d69aa6c54682af586be773017b
lmdrn <leamedrano@gmail.com>
2023-12-19 15:05:04 +0100

    clean clean clean

--- redir_utils.c
+++ redir_utils.c
@@ -6,7 +6,7 @@
 /*   By: lmedrano <lmedrano@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 13:19:27 by lmedrano          #+#    #+#             */
-/*   Updated: 2023/12/19 14:58:22 by lmedrano         ###   ########.fr       */
+/*   Updated: 2023/12/19 15:04:03 by lmedrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,37 +21,14 @@ char	*find_filename(t_commande *cmd)
 	while (curr_arg != NULL)
 	{
 		if (curr_arg->type == 8)
-		{
 			return (file = curr_arg->next->arg);
-			printf("file is %s\n", file);
-		}
 		else if (curr_arg->type == 9)
-		{
 			return (file = curr_arg->next->arg);
-			printf("file is %s\n", file);
-		}
 		else if (curr_arg->type == 10)
-		{
 			return (file = curr_arg->next->arg);
-			printf("file is %s\n", file);
-		}
 		else if (curr_arg->type == 11)
-		{
 			return (file = curr_arg->next->arg);
-			printf("file is %s\n", file);
-		}
 		curr_arg = curr_arg->next;
 	}
 	return (NULL);
-}
-
-char	*create_filename(t_commande *cmd)
-{
-	char	*filename;
-
-	filename = find_filename(cmd);
-	if (filename != NULL)
-		return (filename);
-	else
-		return (NULL);
 }
